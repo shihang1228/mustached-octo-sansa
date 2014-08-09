f59a21224b0be0a0811bcb1003459c7c682a6f07
shihang <study1228.sh@gmail.com>
2014-08-09 18:11:49 +0800

    添加doPost方法，用于调用ContactListServlet中对表单处理的方法

--- src/main/java/com/baldurtech/DispatcherServlet.java
+++ src/main/java/com/baldurtech/DispatcherServlet.java
@@ -10,26 +10,34 @@ public class DispatcherServlet extends HttpServlet
 {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException,ServletException
     {
+        resp.getWriter().println("dispatcherServlet");
         String classPackage = "com.baldurtech.";
         String className = convertUriToClassName(req.getRequestURI());                
-        runAllMethods(classPackage,className);
+        runAllMethods(classPackage,className,req);
     }
     
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException,ServletException
+    {
+        String classPackage = "com.baldurtech.";
+        String className = convertUriToClassName(req.getRequestURI());                
+        runAllMethods(classPackage,className,req);
+    }
+
     public String convertUriToClassName(String uri)
     {
         String arrayUri[] = uri.split("\\/");
-        String className = arrayUri[2].substring(0,1).toUpperCase() + arrayUri[2].substring(1)
-                         + arrayUri[3].substring(0,1).toUpperCase() + arrayUri[3].substring(1) 
+        String className = arrayUri[3].substring(0,1).toUpperCase() + arrayUri[3].substring(1)
+                         + arrayUri[4].substring(0,1).toUpperCase() + arrayUri[4].substring(1) 
                          + "Servlet";
         return className;
     }
     
-    public void runAllMethods(String classPackage, String className)
+    public void runAllMethods(String classPackage, String className,HttpServletRequest req)
     {
         try
         {   
             TestCase testCase = new TestCase();
-            testCase.runAllMethods(Class.forName("com.baldurtech." + className));       
+            testCase.runAllMethods(Class.forName("com.baldurtech." + className),req);       
         }
         catch(Exception ex)
         {
